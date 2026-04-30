1af15391f27b29f9155437088155d8be4efd083f
Maxim LaPlante <maxim.laplante@maine.edu>
2026-04-29 20:18:27 -0400

    [oops] Forgot the cfg file that is very important!

---
+++ UDP_cfg.h
@@ -0,0 +1,12 @@
+#ifndef ADPC_UDP_CFG_H
+#define ADPC_UDP_CFG_H
+
+
+
+#define UDP_PORT 1234
+#define BEACON_TARGET "192.68.4.1"
+
+// Maximum length of our message
+#define BEACON_MSG_LEN_MAX 511
+
+#endif
